39f2d60621414ffab2a7fa9549a369176c469d29
Katrina <32313004+brownkat6@users.noreply.github.com>
2020-07-29 17:40:06 -0400

    Update image_demo.py

--- image_demo.py
+++ image_demo.py
@@ -22,7 +22,7 @@ ROOT_DIR = "/content/tensorflow-yolov3"
 return_elements = ["input/input_data:0", "pred_sbbox/concat_2:0", "pred_mbbox/concat_2:0", "pred_lbbox/concat_2:0"]
 pb_file         = ROOT_DIR + "/yolov3_coco.pb"
 image_path      = ROOT_DIR + "/docs/images/road.jpeg"
-num_classes     = 80
+num_classes     = 1
 input_size      = 416
 graph           = tf.Graph()
 
