1823466a63c4b65f189d957cbe564f1a075d23e3
Katrina <32313004+brownkat6@users.noreply.github.com>
2020-07-30 18:50:19 -0400

    Update freeze_graph.py

--- freeze_graph.py
+++ freeze_graph.py
@@ -17,8 +17,10 @@ from core.yolov3 import YOLOV3
 
 ROOT_DIR = "/content/tensorflow-yolov3"
 
+latest_checkpoint_path = "/content/gdrive/My Drive/Robotics/yolov3/checkpoint/yolov3_test_loss=8.5068.ckpt"
+
 pb_file = ROOT_DIR + "/yolov3_coco.pb"
-ckpt_file = ROOT_DIR + "/checkpoint/yolov3_coco_demo.ckpt"
+ckpt_file = latest_checkpoint_path#ROOT_DIR + "/checkpoint/yolov3_coco_demo.ckpt"
 output_node_names = ["input/input_data", "pred_sbbox/concat_2", "pred_mbbox/concat_2", "pred_lbbox/concat_2"]
 
 with tf.name_scope('input'):
